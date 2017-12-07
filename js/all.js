ad172b18d866015034a4804c73b5dbcfb97d93cb
wxywr <1261046195@qq.com>
2017-12-07 12:24:30 +0800

    update

--- js/all.js
+++ js/all.js
@@ -14,11 +14,42 @@ $(function(){
 	})
 
 	$('.func').mouseover(function(){
-		$('.head .triangle').css('display','block');
+		// $('.head .triangle').css('display','block');
 		$('.head .big').css('display','block');
 	})
 	$('.func').mouseout(function(){
-		$('.head .triangle').css('display','none');
+		// $('.head .triangle').css('display','none');
 		$('.head .big').css('display','none');
 	})
+
+
+	$("input[type='checkbox']").click(function(){
+		if($(this).is(':checked')){
+			$(this).parent().parent().css('backgroundColor','#eee');
+			$(this).parent().siblings('.h').children().eq(1).click(function(){
+				if(window.confirm('确定要删除吗？')){
+		           $(this).parent().parent().remove();
+		            return true;
+		        }else{
+		            return false;
+		        }
+			})
+
+			$(this).parent().siblings('.h').children().eq(0).click(function(){
+				$('.layout').css('display','block');
+			})
+		}
+		else{
+			$(this).parent().parent().css('backgroundColor','#fff');
+		}
+	})
+
+	$('.cancel').click(function(){
+		$('.layout').css('display','none');
+	})
+	$('.refer').click(function(){
+		$('.layout').css('display','none');
+	})
+		
+
 })
